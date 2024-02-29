9a3687830f87aa13e516f12e013e43f8ef3d14c4
John Hardy <jh@lagado.com>
2024-02-29 13:18:06 +1100

    add files

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  mode: 'jit',
+  darkMode: 'media',
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+      }
+    },
+  },
+  plugins: [],
+}
